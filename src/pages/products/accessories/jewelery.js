9946f8667515862be26f9e36f85e05a9584205a9
Meri Badoyan <badoyanmary@gmail.com>
2023-07-16 17:26:42 +0400

    constatns added

--- src/pages/products/accessories/jewelery.js
+++ src/pages/products/accessories/jewelery.js
@@ -1,3 +1,19 @@
 export const Jewelery = () => {
-  return <h1>Jewelery</h1>;
+  const [jewelery, setJewelery] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/category/jewelery`)
+      .then((res) => res.json())
+      .then((json) => setJewelery(json));
+  }, []);
+
+  return (
+    <>
+      <Products
+        products={jewelery}
+        title={"Jewelery"}
+        alterImages={JEWELERY_ALTER_IMAGES}
+      />
+    </>
+  );
 };
---
+++ src/pages/products/constants.js
@@ -0,0 +1,72 @@
+import MenImage from './images/alterImage.jpg';
+import MenImage2 from './images/alterImage2.jpg';
+import MenImage3 from './images/alterImage3.jpg';
+import WomanImage1 from './images/woman1.webp';
+import WomanImage2 from './images/woman2.jpg';
+import WomanImage3 from './images/woman3.jpg';
+import JewlImage1 from './images/jewl1.jpg';
+import JewlImage2 from './images/jewl2.jpg';
+import JewlImage3 from './images/jewl3.jpg';
+import ElectImage1 from './images/elect1.jpg';
+import ElectImage2 from './images/elect2.jpg';
+import ElectImage3 from './images/elect3.jpg';
+
+export const MEN_ALTER_IMAGES = [
+  {
+    id: 1,
+    src: MenImage,
+  },
+  {
+    id: 2,
+    src: MenImage2,
+  },
+  {
+    id: 3,
+    src: MenImage3,
+  },
+];
+
+export const WOMEN_ALTER_IMAGES = [
+  {
+    id: 1,
+    src: WomanImage1,
+  },
+  {
+    id: 2,
+    src: WomanImage2,
+  },
+  {
+    id: 3,
+    src: WomanImage3,
+  },
+];
+
+export const ELECTRONICS_ALTER_IMAGES = [
+  {
+    id: 1,
+    src: ElectImage1,
+  },
+  {
+    id: 2,
+    src: ElectImage2,
+  },
+  {
+    id: 3,
+    src: ElectImage3,
+  },
+];
+
+export const JEWELERY_ALTER_IMAGES = [
+  {
+    id: 1,
+    src: JewlImage1,
+  },
+  {
+    id: 2,
+    src: JewlImage2,
+  },
+  {
+    id: 3,
+    src: JewlImage3,
+  },
+];
