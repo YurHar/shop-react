3279fc10a987fffd9af0d1a6ceb2dad838a86458
Meri Badoyan <badoyanmary@gmail.com>
2023-07-18 22:00:48 +0400

    Signle Product key corrected

--- src/pages/table/ProductTable.js
+++ src/pages/table/ProductTable.js
@@ -1,23 +1,21 @@
 import MainContent from "../../components/layout/main-content";
-import { Table, } from 'antd';
-import { ColumnsType, data } from "./constant"
+import { Table } from "antd";
+import { ColumnsType, data } from "./constant";
 
 const contentStyle = {
-  width: '80%',
+  width: "80%",
   height: "450",
   marginLeft: "auto",
   marginRight: "auto",
-  marginTop: '20px',
+  marginTop: "20px",
 };
 
 export const ProductTable = () => {
-
   return (
     <MainContent>
       <div style={contentStyle}>
         <Table columns={ColumnsType} dataSource={data} />
       </div>
-
     </MainContent>
   );
-}
+};
