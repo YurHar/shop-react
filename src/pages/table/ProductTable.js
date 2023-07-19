927f4a5fea1da3682ae3f6886e51a0dddbf63f25
YurHar <harutyunyan702@gmail.com>
2023-07-19 18:36:05 +0400

    SR-001 Login changes

--- src/pages/table/ProductTable.js
+++ src/pages/table/ProductTable.js
@@ -1,27 +1,43 @@
-import MainContent from "../../components/layout/main-content";
-import { Table, } from 'antd';
+import {Col, Row, Table,} from 'antd';
 import {useColumns} from "./use-column";
-import {useEffect, useState} from "react";
-
-const contentStyle = {
-  width: '80%',
-  height: "450",
-  marginLeft: "auto",
-  marginRight: "auto",
-  marginTop: '20px',
-};
+import Logo from '../../components/layout/images/shop-logo.png'
+import {DefaultFooter} from "../../components/layout/default-footer";
+import React from "react";
+import {LogoutOutlined} from "@ant-design/icons";
+import {useNavigate} from "react-router-dom";
+import {PATHS} from "../../helpers/constant";
 
 export const ProductTable = () => {
+    const columns = useColumns();
+    const navigate = useNavigate();
 
-  const columns = useColumns();
+    const dataWithStatus = JSON.parse(localStorage.getItem("soldItems")).map((item) => ({...item, status: null}));
 
-  const dataWithStatus = JSON.parse(localStorage.getItem("soldItems")).map((item) => ({ ...item, status: 'Accepted' }));
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate(`${PATHS.LOGIN}`);
+    };
 
-  return (
-    <MainContent>
-      <div style={contentStyle}>
-        <Table columns={columns} dataSource={dataWithStatus} />
-      </div>
-    </MainContent>
-  );
+    return (
+        <>
+            <Row align="middle" style={{background: "#958271"}}>
+                <Col span={16} style={{textAlign: 'center'}}>
+                    <img src={Logo} style={{width: 150}} alt=""/>
+                </Col>
+                <Col span={8}>
+                    <LogoutOutlined onClick={() => handleLogout()} style={{
+                        fontSize: '25px',
+                        float: 'right',
+                        marginRight: '60px',
+                    }}/>
+                </Col>
+            </Row>
+            <Row justify={'center'} style={{padding: "69px 0", minHeight: '69.1vh'}}>
+                <Col span={20}>
+                    <Table columns={columns} dataSource={dataWithStatus}/>
+                </Col>
+            </Row>
+            <DefaultFooter/>
+        </>
+    );
 }
