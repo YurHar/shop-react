7ad30c39ecf31a25d45f76eed2ba1e82f7065c98
YurHar <harutyunyan702@gmail.com>
2023-07-20 17:55:41 +0400

    SR-001 count add

--- src/pages/table/ProductTable.js
+++ src/pages/table/ProductTable.js
@@ -1,9 +1,8 @@
-
+import React from "react";
 import {Col, Row, Table,} from 'antd';
 import {useColumns} from "./use-column";
 import Logo from '../../components/layout/images/shop-logo.png'
 import {DefaultFooter} from "../../components/layout/default-footer";
-import React from "react";
 import {LogoutOutlined} from "@ant-design/icons";
 import {useNavigate} from "react-router-dom";
 import {PATHS} from "../../helpers/constant";
@@ -12,7 +11,7 @@ export const ProductTable = () => {
     const columns = useColumns();
     const navigate = useNavigate();
 
-    const dataWithStatus = JSON.parse(localStorage.getItem("soldItems"))?.map((item) => ({...item, status: null}));
+    let data = JSON.parse(localStorage.getItem("soldItems"));
 
     const handleLogout = () => {
         localStorage.removeItem('token');
@@ -30,12 +29,13 @@ export const ProductTable = () => {
                         fontSize: '25px',
                         float: 'right',
                         marginRight: '60px',
+                        color: '#252021',
                     }}/>
                 </Col>
             </Row>
             <Row justify={'center'} style={{padding: "69px 0", minHeight: '69.1vh'}}>
                 <Col span={20}>
-                    <Table columns={columns} dataSource={dataWithStatus}/>
+                    <Table columns={columns} dataSource={data}/>
                 </Col>
             </Row>
             <DefaultFooter/>
